b7bab42b86028a4e4d932b83d07c5189cea0f697
APCS3rd <APCS3rd@MHS-LT9-04>
2019-08-20 10:12:15 -0700

    Complete Hello World Project

---
+++ whats good world/src/HelloWorld.java
@@ -0,0 +1,13 @@
+// Alex David
+//August 19 2019
+//first class in Java
+/* prints a line on the screen*/
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("What's Good World");
+		System.out.println("What's Good World");
+	}
+
+}
