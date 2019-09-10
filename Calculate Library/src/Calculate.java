0743fe2cfd8a269df78b2866314f53416d2edf65
APCS3rd <APCS3rd@MHS-LT9-04>
2019-09-10 11:03:09 -0700

    finish part 2, part 3 up to exponent

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -49,5 +49,53 @@ public class Calculate {
 		int quadraticC = b * d;
 		return quadraticA + variable + "^2" + "+" + quadraticB + variable + "+" + quadraticC;
 	}
+	public static boolean isDivisibleBy(int divided, int divisor) { //checks if the divided is divisble by the divisor
+		boolean isDivisible = divided % divisor == 0;
+		return isDivisible;
+	}
+	public static double absValue (double original) { //find absolute value of original
+		if (original < 0) {
+			return 0 - original;
+		} else {
+			return original;
+		}
+	}
+	public static double max (double number1, double number2) { //return the greater number inputted
+		if (number1 > number2) {
+			return number1;
+		}else{
+			return number2;
+		}
+	}
+	public static double max (double number1, double number2, double number3) { //return the greatest number inputted out of three
+		if (number1 > number2 && number1 > number3) {
+			return number1;
+		}else if(number2 > number1 && number2 > number3) {
+			return number2;
+		}else {
+			return number3;
+		}
+	}
+	public static int min (int number1, int number2) { //return the smaller number inputted
+		if (number1 < number2) {
+			return number1;
+		}else{
+			return number2;
+		}
+	}
+	public static double round2 (double number) { //round number inputted
+		double rounded = (number * 100) + 0.5;
+		double nRounded = (int) rounded;
+		return nRounded/100;
+	}
+	public static double exponent (double exponee, int exponent) { // exponee to the exponent th power
+		double ans = exponee;
+		int n = 1;
+		while (n < exponent) {
+			ans = ans * exponee;
+			n++;
+		}
+		return ans;
+	}
 	
 }
